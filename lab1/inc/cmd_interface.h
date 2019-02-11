95db220e68c3008938da015e060c1e8775f1e2b9
Mitchell <robert.whitlatch@utexas.edu>
2019-02-11 12:05:01 -0600

    g

--- lab1/inc/cmd_interface.h
+++
@@ -1,9 +0,0 @@
-#include "CustomDefinitions.h"
-
-#ifndef __CMD_INTERFACE_H__
-#define __CMD_INTERFACE_H__
-
-// Interface for Message command to ST7735_Message
-int CMD_Message(int argc, char* argv[]);
-
-#endif
